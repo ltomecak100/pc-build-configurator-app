515431e443b34c73cc75a81a78cd335052498444
ltomecak100 <ltomecak12@gmail.com>
2026-03-06 13:44:10 +0100

    Add files via upload

---
+++ src/Konfiguracija.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Treca_zadaca
+{
+	public class Konfiguracija
+	{
+		public int IdKonfiguracije { get; set; }
+		public string Naziv { get; set; }
+		public override string ToString()
+		{
+			return $"{Naziv}";
+		}
+	}
+}
+
