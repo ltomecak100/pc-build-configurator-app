51a7c88e012f05df8e158b1a9be077c438beebd8
ltomecak100 <ltomecak12@gmail.com>
2026-03-06 13:49:24 +0100

    Add files via upload

---
+++ src/Treca_zadaca/Models/Komponenta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treca_zadaca.Models
+{
+    public class Komponenta
+    {
+        public int IdKomponente { get; set; }
+        public string Naziv { get; set; }
+        public string Tip { get; set; }
+        public string Proizvodac { get; set; }
+        public float Cijena { get; set; }
+        public int IdZaposlenika { get; set; }
+        public override string ToString()
+        {
+            return $"{Naziv}({Proizvodac})";
+
+        }
+
+    }
+
+}
---
+++ src/Treca_zadaca/Models/Konfiguracija.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treca_zadaca.Models
+{
+    public class Konfiguracija
+    {
+        public int IdKonfiguracije { get; set; }
+        public string Naziv { get; set; }
+        public override string ToString()
+        {
+            return $"{Naziv}";
+        }
+    }
+
+}
